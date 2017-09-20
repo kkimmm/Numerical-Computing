07e744383612864516aae3e96fb288fb65a472af
にゃんこ せんせい <1187130615@qq.com>
2017-09-20 21:05:56 +0800

    Update main.m

--- lse_SORInterationAlgorithm/main.m
+++ lse_SORInterationAlgorithm/main.m
@@ -1,9 +1,10 @@
+%-------SOR迭代解线性方程组-----------
 clc;clear;
 A = [4 3 0;3 4 -1;0 -1 4]
 b = [24 30 -24]'
 n = length(b)
 x = [1,1,1]'
-w = 1.25;  %w���ɳ����ӣ���w=1ʱ���Է��ִﵽ��ͬ�ľ���Ҫ�󣬵���������Ҫ16�Σ���wҲ����Խ��Խ�� 
+w = 1.25;  %w是松弛因子，当w=1时可以发现达到相同的精度要求，迭代次数需要16次；但w也不是越大越好 
 D = diag(diag(A))
 L = -tril(A,-1)
 U = -triu(A,1)
@@ -14,6 +15,6 @@ for i = 1:100
     if norm(temp - x)<0.0001
         break;
     end
-    fprintf('����%d��֮��',i)
+    fprintf('迭代%d次之后：',i)
     x = temp;
 end
